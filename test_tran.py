18bb015c8ba5683774396a576cd42c2f91b2528b
avrch <61111977+avrch@users.noreply.github.com>
2020-11-14 17:36:30 +0100

    Update test_tran.py

--- test_tran.py
+++ test_tran.py
@@ -4,16 +4,6 @@ import torch
 from torchvision.transforms import functional as F
 
 
-def _flip_coco_person_keypoints(kps, width):
-    flip_inds = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15]
-    flipped_data = kps[:, flip_inds]
-    flipped_data[..., 0] = width - flipped_data[..., 0]
-    # Maintain COCO convention that if visibility == 0, then x, y = 0
-    inds = flipped_data[..., 2] == 0
-    flipped_data[inds] = 0
-    return flipped_data
-
-
 class Compose(object):
     def __init__(self, transforms):
         self.transforms = transforms
@@ -22,18 +12,6 @@ class Compose(object):
         for t in self.transforms:
             image = t(image)
         return image
-
-
-class RandomHorizontalFlip(object):
-    def __init__(self, prob):
-        self.prob = prob
-
-    def __call__(self, image):
-        if random.random() < self.prob:
-            height, width = image.shape[-2:]
-            image = image.flip(-1)
-        return image
-
 
 class ToTensor(object):
     def __call__(self, image):
