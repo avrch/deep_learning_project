e8cd7b61adc8fd52800d9f349c4d24e131617eef
avrch <61111977+avrch@users.noreply.github.com>
2020-11-16 15:55:51 +0100

    r

--- test_tranform.py
+++ test_tranform.py
@@ -4,16 +4,30 @@ import torch
 from torchvision.transforms import functional as F
 
 
+# class Compose(object):
+#     def __init__(self, transforms):
+#         self.transforms = transforms
+
+#     def __call__(self, image):
+#         for t in self.transforms:
+#             image = t(image)
+#         return image
+
 class Compose(object):
     def __init__(self, transforms):
         self.transforms = transforms
 
-    def __call__(self, image):
+    def __call__(self, image, target):
         for t in self.transforms:
-            image = t(image)
-        return image
+            image, target = t(image, target)
+        return image, target
+
+#class ToTensor(object):
+#    def __call__(self, image):
+#        image = F.to_tensor(image)
+#        return image
 
 class ToTensor(object):
-    def __call__(self, image):
+    def __call__(self, image, target):
         image = F.to_tensor(image)
-        return image
+        return image, target
